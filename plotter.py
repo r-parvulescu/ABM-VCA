46c17bd8298dbdb111ebdccea4341570a1c75b4d
Radu Andrei Pârvulescu <rap348@cornell.edu>
2020-11-25 16:52:25 -0500

    split up and clean functions to improve readability, added functions to compare predicted vs. observed vacancy chain lengths

--- plotter.py
+++ plotter.py
@@ -19,53 +19,72 @@ import numpy as np
 import pandas as pd
 from string import punctuation
 from copy import deepcopy
+import csv
 
 
-def make_time_series_figures(batchruns, out_directory, burn_in_steps=0, plot_shock_step=0,
-                             stdev=True, experiment_name=''):
+def make_time_series_figures(batchruns, out_dir, burn_in_steps=0, shock_step=0, stdev=True, experiment_name=''):
     """
     Makes time series figures and save. them to disk
     If input is more than one batchrun, overlay the output of the two batchruns in the same figure, so you can see the
     difference between runs with and without shocks.
+
     NB: if dealing with multiple batchruns, each batchrun must have the exact same parametrisation but for the shocks.
 
-    :param batchruns
-    :param out_directory:
-    :param burn_in_steps:
-    :param plot_shock_step
-    :param stdev:
-    :param experiment_name:
-    :return None
+    NB: if dealing with multple batchruns, each must have identical nesting and key:value structure, o
+        only bottom level data/values may differ
 
+    :param batchruns: list, each of a batchrun, i.e. one model run n-iterations
+    :param out_dir: str, where we want the figures to live
+    :param burn_in_steps: int, number of steps at the beginning of model runs that we want to exclude from the figures
+    :param shock_step: int, when (if at all) we draw a dashed, vertical line to signify the step at which a system
+                            shock occurred; 0 by default, i.e. no shock
+    :param stdev: bool, whether we plot shaded areas around the mean line, representing two standard deviations away
+                  from the mean; False by default
+    :param experiment_name: str, the name of the particular simulation experiment, e.g. "The Big Purge"
+    :return None
     """
 
-    br1_per_step_stats = get_means_std(batchruns[0])
-    if len(batchruns) == 2:
-        br2_per_step_stats = get_means_std(batchruns[1])
-    # look into the metrics
-    # NB: all batchruns must have identical nesting and key:value structure, only bottom level data/values differ
-    for k in br1_per_step_stats.keys():
-        colour_counter = 0
-        for l in br1_per_step_stats[k].keys():  # look into the submetrics
-            br1_mean_line, br1_stdev_line = None, None
-            br2_mean_line, br2_stdev_line = None, None
-            for m in br1_per_step_stats[k][l].keys():  # look into means and stdevs
-                if m == "Mean Across Runs":
-                    br1_mean_line = br1_per_step_stats[k][l][m][burn_in_steps:]  # NB: ignore burn-in steps
-                    if len(batchruns) == 2:
-                        br2_mean_line = br2_per_step_stats[k][l][m][burn_in_steps:]
-                else:
-                    br1_stdev_line = br1_per_step_stats[k][l][m][burn_in_steps:]
-                    if len(batchruns) == 2:
-                        br2_stdev_line = br2_per_step_stats[k][l][m][burn_in_steps:]
-            # plot the line
-            plot_mean_line(k, l, br1_mean_line, br1_stdev_line, colour_counter, "-", stdev)
-            if len(batchruns) == 2:
-                plot_mean_line(k, "shock", br2_mean_line, br2_stdev_line, colour_counter, "--", stdev)
-            colour_counter += 1
+    batchruns_per_step_stats = [get_means_std(br) for br in batchruns]
+    prototype_br_stats = batchruns_per_step_stats[0]
+    for br_per_step_stats in batchruns_per_step_stats:
+        # look into the metrics
+        for metric_name in prototype_br_stats.keys():
+            colour_counter = 0
+            for line_name in prototype_br_stats[metric_name].keys():  # look into the submetrics
+                b_run_mean_line, b_run_stdev_line = get_batch_run_mean_stdev_lines(br_per_step_stats, metric_name,
+                                                                                   line_name, burn_in_steps)
+                # plot the lines
+                plot_mean_line(shock_step, metric_name, line_name, b_run_mean_line, b_run_stdev_line, colour_counter,
+                               "-", stdev)
+                colour_counter += 1
 
-        # name the plot, save the figure to disk
-        save_figure(k, batchruns[0], out_directory, plot_shock_step=plot_shock_step, experiment_name=experiment_name)
+            # name the plot, save the figure to disk
+            save_figure(metric_name, batchruns[0], out_dir, shock_step=shock_step, experiment_name=experiment_name)
+
+
+def get_batch_run_mean_stdev_lines(batch_run_per_step_stats, metric_name, line_name, burn_in_steps):
+    """
+    Get the mean or stdev line (as a list) of per-step metric values averaged or stdeve'd across all model
+    iterations. So each value of the list is e.g. an average of metric values at that step, across all model iterations,
+    and the values are in the order of model steps, e.g. 1,2,3...
+
+    :param batch_run_per_step_stats:
+    :param metric_name: str, the name of a metric, e.g. average_vacancy_chain_length
+    :param line_name: str, the name of a particular line in that metric, e.g. "Level 1"; this is important because
+                      most metrics are disaggregated, usually by hierarchical level, and we want to plot multiple of
+                      these lines in one graph.
+    :param burn_in_steps: the number of steps from the beginning of the model that we DO NOT want to see
+    :return: a 2-tuple of lists, the mean line and stdev line for that metric, for that line
+    """
+    br_mean_line, br_stdev_line = None, None
+    # NB: "measure type" is either "Mean Across Runs" or "StDev Across Runs"
+    for measure_type in batch_run_per_step_stats[metric_name][line_name].keys():
+        # NB: ignore burn-in steps
+        if measure_type == "Mean Across Runs":
+            br_mean_line = batch_run_per_step_stats[metric_name][line_name][measure_type][burn_in_steps:]
+        else:
+            br_stdev_line = batch_run_per_step_stats[metric_name][line_name][measure_type][burn_in_steps:]
+    return br_mean_line, br_stdev_line
 
 
 # HELPERS FOR TRANSFORMING TIME SERIES DATA
@@ -162,55 +181,131 @@ def flatten_dicts_into_df(input_dict, output_dict, output_dict_key):
 
 # HELPERS FOR PLOTTING
 
-# plotting helper for make_time_series_figures
-def plot_mean_line(metric_name, line_name, mean_line, stdev_line, colour_counter, linestyle, stdev=True):
-    """given line name, mean and associated stdev values, and a colour counter, plots a line"""
-    #if "shock" in metric_name:  # since cohorts vanish in max 30 years, need a tight window for a useful plot
-    #    mean_line = mean_line[15:-24]
-    #    stdev_line = stdev_line[15:-24]
-    colours = ['r-', 'b-', 'k-', 'g-', 'c-', 'm-', 'y-']
+def plot_mean_line(shock_step, metric_name, line_name, mean_line, stdev_line, colour_counter, linestyle, stdev=True):
+    """Plot a mean line, and optionally a shaded area of two standard deviations around the mean."""
+
+    # since cohorts have total turnover in 30 years, use only the thirty years after the shock step (inclusive)
+    if metric_name == "percent_actors_from_before_first_shock":
+        mean_line = mean_line[shock_step:shock_step+31]
+        stdev_line = stdev_line[shock_step:shock_step+31]
+
+    colours = ['r-', 'b-', 'g-', 'k-', 'y-', 'c-', 'm-']
     x = np.linspace(0, len(mean_line) - 1, len(mean_line))
     plt.plot(x, mean_line, colours[colour_counter], linestyle=linestyle, label=line_name)
+
     if stdev:
         # make sure lower stdev doesn't go below zero
         stdev_lowbound = mean_line - stdev_line * 2
         stdev_lowbound[stdev_lowbound < 0] = 0
+
         # and if dealing with percentages, that higher stdev line doesn't go above 100
         stdev_highbound = mean_line + stdev_line * 2
         if "percent" in metric_name:
             stdev_highbound[stdev_highbound > 100] = 100
-        plt.fill_between(x, stdev_lowbound, stdev_highbound, alpha=0.2)
-    y_axis_labels = {"average_career_length": "years",
-                     "average_vacancy_chain_length": "steps",
-                     "percent_female_actors": "percent",
-                     "percent_actors_from_before_first_shock": "percent",
-                     "get_count_vacancies_in_system": "count",
-                     "get_count_vacancies_per_step": "count",
-                     "get_actor_counts": "count",
-                     "get_agent_sets_sizes": "count"}
-    plt.ylabel(y_axis_labels[metric_name])
+
+        plt.fill_between(x, stdev_lowbound, stdev_highbound, color=colours[colour_counter][0], alpha=0.2)
 
 
 # plotting helper for make_time_series_figures
-def save_figure(metric_name, batchrun, out_directory, plot_shock_step=0, experiment_name=''):
-    """given a title for the figure and a batchrun, completes figure, saves it to .png, and closes open plots"""
+def save_figure(metric_name, batchrun, out_dir, shock_step=0, experiment_name=''):
+    """Complete a figure with titles, legend, extra line-markers and grid, then saves the figure to disk."""
+
+    y_axis_labels = {"average_career_length": "years", "average_vacancy_chain_length": "steps",
+                     "percent_female_actors": "percent", "percent_actors_from_before_first_shock": "percent",
+                     "get_count_vacancies_in_system": "count", "get_count_vacancies_per_step": "count",
+                     "get_actor_counts": "count", "get_agent_sets_sizes": "count"}
+
     # if no title name provided, use function name
     if experiment_name:
         plt.title(str(experiment_name + '\n' + metric_name.replace('_', ' ')).title())
     else:
         plt.title(metric_name.replace('_', ' ').title())
+
+    #
     ax = plt.subplot(111)
     box = ax.get_position()
     ax.set_position([box.x0, box.y0 + box.height * 0.1,
                      box.width, box.height * 0.9])
-    # unless we're dealing with the before-first-shock metrics (which are plotted differently)
-    # add a dashed, black line at the first shock step
-    #if "shock" not in metric_name:
-    #    ax.axvline(x=plot_shock_step, lw=2, color='k', linestyle='--')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), fancybox=True, shadow=True, ncol=5, fontsize='medium')
+
+    # if there is a shock, draw a vertical, dashed line indicating at which step the shock occurred
+    if shock_step:
+        ax.axvline(x=shock_step, lw=2, color='k', linestyle='--')
+
+    # add the legend, label the x and y axes, and add gridlines
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.14), fancybox=True, shadow=True, ncol=5, fontsize='medium')
     plt.xlabel("periods")
-    figure_filename = out_directory + metric_name + "_" + str(batchrun.iterations) + "runs_" + \
-                      str(batchrun.max_steps) + "steps.png"
-    plt.grid()  # add gridlines
+    plt.ylabel(y_axis_labels[metric_name])
+    plt.grid()
+
+    # name and save the figure
+    iterations, steps = str(batchrun.iterations), str(batchrun.max_steps)
+    figure_filename = out_dir + metric_name + "_" + iterations + "runs_" + steps + "steps.png"
     plt.savefig(figure_filename)
+
     plt.close()
+
+
+# HELPERS FOR TABLE METRICS
+
+def theoretical_observed_chain_length(vacancy_transition_probability_matrix, batchruns, out_dir):
+    """
+    Given the inter-level vacancy transition matrix used to simulate the vacancy dynamics, compare the chain length
+    as predicted by a Markov-chain model to the observed chain length, averaged across model steps and iterations.
+    Results are disaggregated by the state in which the chain started, i.e. comparing predicted vs. observed lengths
+    of chain started in level 1.
+
+    :param vacancy_transition_probability_matrix: list of lists, the matrix showing the transition probabilities
+                                                  matrix of vacancies. The form is as in the example below
+
+                                                             Level 1     Level 2     Level 3     Retire
+                                                  Level 1    [[0.3,         0.4,        0.1,        0.2],
+                                                  Level 2     [0.1,         0.4,        0.4,        0.1],
+                                                  Level 3     [0.05,        0.05,       0.2,        0.7]]
+
+    :param batchruns: a list of batchruns, where each batchrun is the same model run in n-iterations
+    :param out_dir: str, directory where we want the comparison table to live
+    """
+
+    # by default, consider all steps, i.e. don't throw away the burn-ins
+    burn_in_steps = 0
+    for b_run in batchruns:
+        predicted_chain_lengths = markov_predicted_chain_length(vacancy_transition_probability_matrix)
+
+        observed_average_chain_lengths = []
+        b_run_per_step_stats = get_means_std(b_run)
+        for line_name in b_run_per_step_stats["average_vacancy_chain_length"].keys():
+            b_run_mean_line = get_batch_run_mean_stdev_lines(b_run_per_step_stats, "average_vacancy_chain_length",
+                                                             line_name, burn_in_steps)[0]
+            observed_average_chain_lengths.append(np.mean(b_run_mean_line))
+
+    # save to disk the comparisons, in a csv table
+        with open(out_dir + "theoretical_vs_simulated_chain_lengths.csv", "w") as out_f:
+            writer = csv.writer(out_f)
+            writer.writerow(["Level", "Markov Chain Predictions", "Average of Observed Chain Lengths"])
+            for i in range(0, len(predicted_chain_lengths)):
+                writer.writerow([i, predicted_chain_lengths[i], observed_average_chain_lengths[i]])
+
+
+def markov_predicted_chain_length(vacancy_transition_probability_matrix):
+    """
+    Test if the per-level chain length predicted by a first-order, discrete, embedded Markov-chain with absorbing
+    states lines up with your observed average chain length.
+
+    NB: we calculate predicted chain length, disaggregated by the level in which the chain starts, by first creating
+    the matrix N = inv(I - Q), where Q is the square submatrix of the vacancy transition matrix excluding absorbing
+    states (i.e. the retirement column), I is the identity matrix of similar dimensions, and "inv" signifies inversion.
+    To get predicted chain length according to the vacancy's starting level we take Nx1, where 1 a vector of ones.
+
+    :param vacancy_transition_probability_matrix:
+    """
+    # get the square transition probability submatrix, ignoring the absorbing states, by convention the last column
+    trans_prob_submatrix = np.array([row[:-1] for row in vacancy_transition_probability_matrix])
+    number_of_levels = len(trans_prob_submatrix)
+    identity = np.identity(number_of_levels)
+    identity_minus_trans_prob_submatrix = np.subtract(identity, trans_prob_submatrix)
+    # make the N matrix
+    n_matrix = np.linalg.inv(identity_minus_trans_prob_submatrix)
+    vector_of_ones = np.ones(number_of_levels)
+    # get the predicted chain lengths, or in White's (1970) jargon, the "multiplier"
+    pred_chain_lengths = np.matmul(n_matrix, vector_of_ones)
+    return pred_chain_lengths
