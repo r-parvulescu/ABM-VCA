d11d4286f6902c16ca8ccb9a37a45b29aef4dc1b
Radu Andrei Pârvulescu <rap348@cornell.edu>
2020-11-27 15:23:45 -0500

    updated with toy example and new functions

--- experiment.py
+++ experiment.py
@@ -15,6 +15,7 @@ whites_methodist_church = {"positions_per_level": [20, 40, 80],
                            "vacancy_trans_prob_matrix": [[.46, .33, .05, .16],
                                                          [.11, .41, .12, .36],
                                                          [.02, .16, .24, .58]],
+                           "vacancy_benefit_deficit_matrix": [],
                            "firing_schedule": {"steps": {}, "actor retirement probs": []},
                            "growth_orders": {"steps": {}, "level growths": {}},
                            "start_percent_female": 0.,
@@ -33,23 +34,48 @@ ro_judges_2014 = {"positions_per_level": [10, 100, 150, 200],
                                                 [0., .06, .68, .1, .16],
                                                 [0., 0., .21, .66, .13],
                                                 [0., .01, .02, .36, .61]],
+                  "vacancy_benefit_deficit_matrix": [],
                   "firing_schedule": {"steps": {}, "actor retirement probs": []},
                   "growth_orders": {"steps": {}, "level growths": {}},
                   "start_percent_female": 0.,
                   "percent_female_entry_per_step": fem_entry_per_step,
                   }
 
+# toy example, featuring no downward mobility of people (therefore only downward mobility of vacancies)
+total_steps = 1500
+fem_entry_per_step = {i: 0 for i in range(total_steps)}
+toy_outdir = out_dir + 'toy/'
+toy_exp_name = "Toy Example"
+toy = {"positions_per_level": [20, 40, 80],
+       "actor_retire_probs": [0.1, 0.075, 0.05],
+       "vacancy_trans_prob_matrix": [[.4, .4, .1, .1],
+                                     [0., .4, .3, .3],
+                                     [0., 0., .4, .6]],
+       "vacancy_benefit_deficit_matrix": [[1, 2, 3, 4],
+                                          [-1, 3, 4, 5],
+                                          [-2, -1, 5, 6]],
+       "firing_schedule": {"steps": {}, "actor retirement probs": []},
+       "growth_orders": {"steps": {}, "level growths": {}},
+       "start_percent_female": 0.,
+       "percent_female_entry_per_step": fem_entry_per_step}
+
 if __name__ == "__main__":
 
     data_collector = {"average_career_length": collector.get_average_actor_career_length,
                       "average_vacancy_chain_length": collector.get_average_vacancy_chain_length,
-                      "get_count_vacancies_per_step": collector.get_count_vacancies_per_step,
+                      "count_vacancies_per_step": collector.get_count_vacancies_per_step,
+                      "actor_turnover_rate": collector.get_actor_turnover_rate,
+                      "time_to_promotion_from_last_level": collector.get_time_to_promotion_from_last_level,
+                      "time_to_retirement_from_last_level": collector.get_time_to_retirement_from_last_level,
+                      "net_vacancy_effects": collector.get_net_vacancy_effects
                       }
 
     iterations = 5
 
     experiments = [(whites_methodist_church, whites_methodist_church_outdir, whites_methodist_churh_exp_name),
-                   (ro_judges_2014, ro_judges_2014_outdir, ro_judges_2014_exp_name)]
+                   (ro_judges_2014, ro_judges_2014_outdir, ro_judges_2014_exp_name),
+                   (toy, toy_outdir, toy_exp_name)
+                   ]
 
     for exp in experiments:
         variable_params, fixed_params = None, exp[0]
@@ -57,10 +83,11 @@ if __name__ == "__main__":
         fixed_params["shock_step"] = 0
         fixed_params["vacancy_move_period"] = 15
 
-        burn_in_steps = 0
-        num_start_steps_to_show = 0
+        burn_in_steps = 20
         out_directory = exp[1]
         model_reporters = {"Data Collector": lambda m: m.data_collector}
+
+        measures = data_collector.keys()
 
         simulation = model.VacancyChainAgentBasedModel
 
@@ -68,6 +95,7 @@ if __name__ == "__main__":
                                                       total_steps, model_reporters)
 
         plotter.make_time_series_figures(b_runs, out_directory, burn_in_steps=burn_in_steps,
-                                         shock_step=num_start_steps_to_show, experiment_name=exp[2], stdev=True)
+                                         shock_step=fixed_params["shock_step"], experiment_name=exp[2], stdev=True)
 
-        tables.theoretical_observed_chain_length(fixed_params["vacancy_trans_prob_matrix"], b_runs, out_directory)
+        tables.make_convergence_table(b_runs, measures, out_directory, fixed_params["vacancy_trans_prob_matrix"],
+                                      burn_in_steps=burn_in_steps)
