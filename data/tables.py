8fcd757a1fdd81ffed3e51891b85d00f4ff7cc53
Radu Andrei Pârvulescu <rap348@cornell.edu>
2020-11-27 15:22:17 -0500

    can now make table with convergence results

--- data/tables.py
+++ data/tables.py
@@ -8,42 +8,46 @@ import numpy as np
 import csv
 
 
-def theoretical_observed_chain_length(vacancy_transition_probability_matrix, batchruns, out_dir, burn_in_steps=0):
+def make_convergence_table(batchruns, measures, out_dir, vacancy_transition_probability_matrix, burn_in_steps=0):
     """
-    Given the inter-level vacancy transition matrix used to simulate the vacancy dynamics, compare the chain length
-    as predicted by a Markov-chain model to the observed chain length, averaged across model steps and iterations.
-    Results are disaggregated by the state in which the chain started, i.e. comparing predicted vs. observed lengths
-    of chain started in level 1.
+    Make a table in which you report the values of measures averaged across equilibrium (i.e. post-burn-in) steps.
 
-    :param vacancy_transition_probability_matrix: list of lists, the matrix showing the transition probabilities
-                                                  matrix of vacancies. The form is as in the example below
-
-                                                             Level 1     Level 2     Level 3     Retire
-                                                  Level 1    [[0.3,         0.4,        0.1,        0.2],
-                                                  Level 2     [0.1,         0.4,        0.4,        0.1],
-                                                  Level 3     [0.05,        0.05,       0.2,        0.7]]
-
-    :param batchruns: a list of batchruns, where each batchrun is the same model run in n-iterations
+    :param batchruns:
+    :param measures: dict, where keys are the (str) names of the measures that we're including in the name
     :param out_dir: str, directory where we want the comparison table to live
+    :param vacancy_transition_probability_matrix: list of lists, the matrix showing the transition probabilities
+                                              matrix of vacancies. The form is as in the example below
+
+                                                         Level 1     Level 2     Level 3     Retire
+                                              Level 1    [[0.3,         0.4,        0.1,        0.2],
+                                              Level 2     [0.1,         0.4,        0.4,        0.1],
+                                              Level 3     [0.05,        0.05,       0.2,        0.7]]
+
     :param burn_in_steps: int, how many beginning steps we treat as the model hitting convergence (i.e. "burning in")
                           and therefore don't consider for the metric calculations. Default is 0, i.e. no burn-in.
     """
 
-    # by default, consider all steps, i.e. don't throw away the burn-ins
-    for b_run in batchruns:
-        predicted_chain_lengths = collector.markov_predicted_chain_length(vacancy_transition_probability_matrix)
+    vals = {i: {measure_name: {"measure": {}, "stdev": {}} for measure_name in measures} for i in range(len(batchruns))}
 
-        observed_average_chain_lengths = []
-        b_run_per_step_stats = helpers.get_means_std(b_run)
-        for line_name in b_run_per_step_stats["average_vacancy_chain_length"].keys():
-            b_run_mean_line = helpers.get_batch_run_mean_stdev_lines(b_run_per_step_stats,
-                                                                     "average_vacancy_chain_length", line_name,
-                                                                     burn_in_steps)[0]
-            observed_average_chain_lengths.append(np.mean(b_run_mean_line))
+    for idx, br in enumerate(batchruns):
+        b_run_per_step_stats = helpers.get_means_std(br)
+        for measure_name in measures:
+            for line_name in b_run_per_step_stats[measure_name].keys():
+                mean_stdev = helpers.get_batch_run_mean_stdev_lines(b_run_per_step_stats, measure_name, line_name,
+                                                                    burn_in_steps=burn_in_steps)
+                vals[idx][measure_name]["measure"][line_name] = np.mean(mean_stdev[0])
+                vals[idx][measure_name]["stdev"][line_name] = np.mean(mean_stdev[1])
 
-        # save to disk the comparisons, in a csv table
-        with open(out_dir + "theoretical_vs_simulated_chain_lengths.csv", "w") as out_f:
-            writer = csv.writer(out_f)
-            writer.writerow(["Level", "Markov Chain Predictions", "Average of Observed Chain Lengths"])
-            for i in range(0, len(predicted_chain_lengths)):
-                writer.writerow([i, predicted_chain_lengths[i], observed_average_chain_lengths[i]])
+    # save table to disk
+    with open(out_dir + "equilibrium_values.csv", "w") as out_f:
+        writer = csv.writer(out_f)
+        writer.writerow(["Batchrun", "Measure Name", "Mean or StDev", "Level", "Value"])
+        for br in vals:
+            for measure_name in vals[br]:
+                for measure_type in vals[br][measure_name]:
+                    for line_name in vals[br][measure_name][measure_type]:
+                        writer.writerow([br, measure_name, measure_type, line_name,
+                                         vals[br][measure_name][measure_type][line_name]])
+
+        for lvl, value in collector.markov_predicted_chain_length(vacancy_transition_probability_matrix).items():
+            writer.writerow(["", "Markov Predicted Vacancy Chain Length", "", lvl, value])
