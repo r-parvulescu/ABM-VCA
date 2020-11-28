fcaebdc4823e2805ec3c7304a718fa836f10cd8e
Radu Andrei Pârvulescu <rap348@cornell.edu>
2020-11-28 12:00:07 -0500

    batchrun timeseries now have different linestyles

--- data/timeseries.py
+++ data/timeseries.py
@@ -9,14 +9,11 @@ from data import helpers
 
 def make_time_series_figures(batchruns, out_dir, burn_in_steps=0, shock_step=0, stdev=True, experiment_name=''):
     """
-    Makes time series figures and save. them to disk
-    If input is more than one batchrun, overlay the output of the two batchruns in the same figure, so you can see the
-    difference between runs with and without shocks.
+    Makes time series figures and save them to disk. If the input is more than one batchrun, overlay the output of the
+    two batchruns in the same figure, so you can see the difference between batchruns.
 
-    NB: if dealing with multiple batchruns, each batchrun must have the exact same parametrisation but for the shocks.
-
-    NB: if dealing with multple batchruns, each must have identical nesting and key:value structure, o
-        only bottom level data/values may differ
+    NB: if dealing with multiple batchruns, each must have identical nesting and key:value structure, only bottom level
+        data/values may differ
 
     :param batchruns: list, each of a batchrun, i.e. one model run n-iterations
     :param out_dir: str, where we want the figures to live
@@ -28,33 +25,39 @@ def make_time_series_figures(batchruns, out_dir, burn_in_steps=0, shock_step=0, 
     :param experiment_name: str, the name of the particular simulation experiment, e.g. "The Big Purge"
     :return None
     """
-    for br in batchruns:
-        br_per_step_stats = helpers.get_means_std(br)
-        # look into the metrics
-        for metric_name in br_per_step_stats.keys():
+
+    linestyles = ['-', '--', '-.', ':']
+
+    batch_runs_per_step_stats = [helpers.get_means_std(br) for br in batchruns]
+
+    metric_names = batch_runs_per_step_stats[0].keys()
+
+    for m_name in metric_names:
+
+        for idx, br_per_step_stats in enumerate(batch_runs_per_step_stats):
+
             colour_counter = 0
-            for line_name in br_per_step_stats[metric_name].keys():  # look into the submetrics
-                b_run_mean_line, b_run_stdev_line = helpers.get_batch_run_mean_stdev_lines(br_per_step_stats,
-                                                                                           metric_name, line_name,
-                                                                                           burn_in_steps)
+
+            for line_name in br_per_step_stats[m_name].keys():
+                b_run_mean_line, b_run_stdev_line = helpers.get_brun_mean_stdev_lines(br_per_step_stats, m_name,
+                                                                                      line_name, burn_in_steps)
                 # plot the lines
-                plot_mean_line(shock_step, metric_name, line_name, b_run_mean_line, b_run_stdev_line, colour_counter,
-                               "-", stdev)
+                plot_mean_line(shock_step, m_name, line_name, b_run_mean_line, b_run_stdev_line, colour_counter,
+                               linestyles[idx], stdev)
                 colour_counter += 1
 
-            # name the plot, save the figure to disk
-            save_figure(metric_name, batchruns[0], out_dir, shock_step=shock_step, experiment_name=experiment_name)
+        # name the plot, save the figure to disk
+        save_figure(m_name, batchruns[0], out_dir, shock_step=shock_step, experiment_name=experiment_name)
 
 
 def plot_mean_line(shock_step, metric_name, line_name, mean_line, stdev_line, colour_counter, linestyle, stdev=True):
     """Plot a mean line, and optionally a shaded area of two standard deviations around the mean."""
-
     # since cohorts have total turnover in 30 years, use only the thirty years after the shock step (inclusive)
     if metric_name == "percent_actors_from_before_first_shock":
         mean_line = mean_line[shock_step:shock_step+31]
         stdev_line = stdev_line[shock_step:shock_step+31]
 
-    colours = ['r-', 'b-', 'g-', 'k-', 'y-', 'c-', 'm-']
+    colours = ['r', 'b', 'g', 'k', 'y', 'c', 'm']
     x = np.linspace(0, len(mean_line) - 1, len(mean_line))
     plt.plot(x, mean_line, colours[colour_counter], linestyle=linestyle, label=line_name)
 
@@ -77,7 +80,8 @@ def save_figure(metric_name, batchrun, out_dir, shock_step=0, experiment_name=''
     y_axis_labels = {"average_career_length": "years", "average_vacancy_chain_length": "steps",
                      "percent_female_actors": "percent", "percent_actors_from_before_first_shock": "percent",
                      "count_vacancies_in_system": "count", "count_vacancies_per_step": "count",
-                     "actor_counts": "count", "agent_sets_sizes": "count", "actor_turnover_rate": "rate per actor step",
+                     "actor_counts": "count", "agent_sets_sizes": "count",
+                     "actor_turnover_rate": "turnover per actor step",
                      "time_to_promotion_from_last_level": "actor steps",
                      "time_to_retirement_from_last_level": "actor steps",
                      "net_vacancy_effects": "unit"}
